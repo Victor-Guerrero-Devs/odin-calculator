907735b68d2ff3d9915e2837e7aecc3925ccec52
Victor-Guerrero-Devs <Vicguerrero.dev@gmail.com>
2022-12-24 23:21:00 -0600

    Finish app logic

--- scripts/app.js
+++ scripts/app.js
@@ -1,2 +1,22 @@
-const btns = document.querySelectorAll('.calculator__btns');
+const btns = document.querySelectorAll('button');
 const screenEle = document.querySelector('.calculator__display');
+
+let valuesArr = [];
+let calculationsArguments;
+
+function calculate(btn) {
+  const btnValue = btn.textContent;
+
+  if (btnValue === 'CLEAR') {
+    valuesArr = [];
+    screenEle.textContent = '';
+  } else if (btnValue === '=') {
+    screenEle.textContent = eval(calculationsArguments);
+  } else {
+    valuesArr.push(btnValue);
+    calculationsArguments = valuesArr.join('');
+    screenEle.textContent = calculationsArguments;
+  }
+}
+
+btns.forEach((btn) => btn.addEventListener('click', () => calculate(btn)));
